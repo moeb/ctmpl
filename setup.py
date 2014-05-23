f789b1995df91cab7f28b4da5e2eb78f1ff9a36a
moeb <bmoellering@googlemail.com>
2014-05-23 18:08:50 +0200

    added the program and the install routine

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name="ctmpl",
+      version="0.1",
+      description="templates for c",
+      author="Birger Möllering",
+      author_email="bmoellering@googlemail.com",
+      url="https://github.com/moeb/ctmpl",
+      #packages=['ctmpl'])
+      scripts=['ctmpl/ctmpl'])
